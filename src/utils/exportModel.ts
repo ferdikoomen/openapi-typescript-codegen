be9a59b47217eece051f1c5c854683c076d1f4ca
Ferdi Koomen <info@madebyferdi.com>
2020-01-13 22:13:27 +0100

    - Removed sorting of props

--- src/utils/exportModel.ts
+++ src/utils/exportModel.ts
@@ -15,28 +15,11 @@ export function exportModel(model: Model): Model {
                 const nameB = b.toLowerCase();
                 return nameA.localeCompare(nameB);
             }),
-        properties: model.properties.sort((a, b) => {
-            const nameA = a.name.toLowerCase();
-            const nameB = b.name.toLowerCase();
-            return nameA.localeCompare(nameB);
+        enums: model.enums.filter((property, index, arr) => {
+            return arr.findIndex(item => item.name === property.name) === index;
         }),
-        enums: model.enums
-            .filter((property, index, arr) => {
-                return arr.findIndex(item => item.name === property.name) === index;
-            })
-            .sort((a, b) => {
-                const nameA = a.name.toLowerCase();
-                const nameB = b.name.toLowerCase();
-                return nameA.localeCompare(nameB);
-            }),
-        enum: model.enum
-            .filter((enumerator, index, arr) => {
-                return arr.findIndex(item => item.name === enumerator.name) === index;
-            })
-            .sort((a, b) => {
-                const nameA = a.name.toLowerCase();
-                const nameB = b.name.toLowerCase();
-                return nameA.localeCompare(nameB);
-            }),
+        enum: model.enum.filter((enumerator, index, arr) => {
+            return arr.findIndex(item => item.name === enumerator.name) === index;
+        }),
     };
 }
--- src/utils/exportService.ts
+++ src/utils/exportService.ts
@@ -16,20 +16,14 @@ export function exportService(service: Service): Service {
                 const nameB = b.toLowerCase();
                 return nameA.localeCompare(nameB);
             }),
-        operations: service.operations
-            .map(operation => {
-                const name = operation.name;
-                const index = names.get(name) || 0;
-                if (index > 0) {
-                    operation.name = `${name}${index}`;
-                }
-                names.set(name, index + 1);
-                return operation;
-            })
-            .sort((a, b) => {
-                const nameA = a.name.toLowerCase();
-                const nameB = b.name.toLowerCase();
-                return nameA.localeCompare(nameB);
-            }),
+        operations: service.operations.map(operation => {
+            const name = operation.name;
+            const index = names.get(name) || 0;
+            if (index > 0) {
+                operation.name = `${name}${index}`;
+            }
+            names.set(name, index + 1);
+            return operation;
+        }),
     };
 }
