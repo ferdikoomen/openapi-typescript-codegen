9fb81ae4e368775f82d84280a4c818718a2208db
İsmail Codar <icodar@hotmail.com>
2020-03-06 19:07:09 +0300

    propertyRequired with definition.required === true

--- src/openApi/v2/parser/getModelProperties.ts
+++ src/openApi/v2/parser/getModelProperties.ts
@@ -10,7 +10,7 @@ export function getModelProperties(openApi: OpenApi, definition: OpenApiSchema):
     for (const propertyName in definition.properties) {
         if (definition.properties.hasOwnProperty(propertyName)) {
             const property = definition.properties[propertyName];
-            const propertyRequired = definition.required && definition.required.includes(propertyName);
+            const propertyRequired = definition.required === true || (definition.required && definition.required.includes(propertyName));
             if (property.$ref) {
                 const model = getType(property.$ref);
                 models.push({
