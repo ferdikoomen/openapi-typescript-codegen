b437eeb0dc0027207d992fe6f66eef09e28923aa
Ferdi Koomen <info@madebyferdi.com>
2020-05-23 19:40:21 +0200

    - Aligned sorter with 0.2.7, to prevent to many issues with exsiting users

--- src/openApi/v3/parser/sortByRequired.ts
+++ src/openApi/v3/parser/sortByRequired.ts
@@ -1,27 +1,9 @@
 import { OperationParameter } from '../../../client/interfaces/OperationParameter';
 
-/**
- * Sort by required and default values creating the following order:
- * 1. Parameters that are required and have no default value
- * 2. Parameters that are optional and have no default value
- * 3. Parameters that are required and have a default value
- * 4. Parameters that are optional and have a default value
- *
- * Resulting in the following order:
- *
- * function myFunction(param1: string, param2?: string, param3: string = 'hello') {
- *    ...
- * }
- *
- * @param a
- * @param b
- */
 export function sortByRequired(a: OperationParameter, b: OperationParameter): number {
-    const aHasDefaultValue = a.default !== undefined;
-    const bHasDefaultValue = b.default !== undefined;
-    if (aHasDefaultValue && !bHasDefaultValue) return 1;
-    if (!aHasDefaultValue && bHasDefaultValue) return -1;
-    if (a.isRequired && !b.isRequired) return -1;
-    if (!a.isRequired && b.isRequired) return 1;
+    const aNeedsValue = a.isRequired && a.default === undefined;
+    const bNeedsValue = a.isRequired && a.default === undefined;
+    if (aNeedsValue && !bNeedsValue) return -1;
+    if (!aNeedsValue && bNeedsValue) return 1;
     return 0;
 }
