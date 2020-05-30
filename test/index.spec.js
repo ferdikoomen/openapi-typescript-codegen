75aee5f577685c827b85b2fe258b8d941081f7a1
Ferdi Koomen <info@madebyferdi.com>
2020-05-30 13:22:15 +0200

    - Checking order of tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,50 +4,50 @@ const OpenAPI = require('../dist');
 const glob = require('glob');
 const fs = require('fs');
 
-describe('generation', () => {
-    describe('v2', () => {
-        it('generated v2', async () => {
-            await OpenAPI.generate({
-                input: './test/mock/v2/spec.json',
-                output: './test/result/v2/',
-                httpClient: OpenAPI.HttpClient.FETCH,
-                useOptions: true,
-                useUnionTypes: true,
-                exportCore: true,
-                exportSchemas: true,
-                exportModels: true,
-                exportServices: true,
-            });
-        });
-
-        const files = glob.sync('./test/result/v2/**/*.ts');
-
-        test.each(files.map(file => [file]))('file(%s)', file => {
-            const content = fs.readFileSync(file, 'utf8').toString();
-            expect(content).toMatchSnapshot(file);
+describe('v2', () => {
+    it('should generate', async () => {
+        await OpenAPI.generate({
+            input: './test/mock/v2/spec.json',
+            output: './test/result/v2/',
+            httpClient: OpenAPI.HttpClient.FETCH,
+            useOptions: true,
+            useUnionTypes: true,
+            exportCore: true,
+            exportSchemas: true,
+            exportModels: true,
+            exportServices: true,
         });
     });
+});
 
-    describe('v3', () => {
-        it('generated v3', async () => {
-            await OpenAPI.generate({
-                input: './test/mock/v3/spec.json',
-                output: './test/result/v3/',
-                httpClient: OpenAPI.HttpClient.FETCH,
-                useOptions: true,
-                useUnionTypes: true,
-                exportCore: true,
-                exportSchemas: true,
-                exportModels: true,
-                exportServices: true,
-            });
+describe('v3', () => {
+    it('should generate', async () => {
+        await OpenAPI.generate({
+            input: './test/mock/v3/spec.json',
+            output: './test/result/v3/',
+            httpClient: OpenAPI.HttpClient.FETCH,
+            useOptions: true,
+            useUnionTypes: true,
+            exportCore: true,
+            exportSchemas: true,
+            exportModels: true,
+            exportServices: true,
         });
+    });
+});
 
-        const files = glob.sync('./test/result/v3/**/*.ts');
+describe('v2 (snapshot)', () => {
+    const files = glob.sync('./test/result/v2/**/*.ts');
+    test.each(files.map(file => [file]))('file(%s)', file => {
+        const content = fs.readFileSync(file, 'utf8').toString();
+        expect(content).toMatchSnapshot(file);
+    });
+});
 
-        test.each(files.map(file => [file]))('file(%s)', file => {
-            const content = fs.readFileSync(file, 'utf8').toString();
-            expect(content).toMatchSnapshot(file);
-        });
+describe('v3 (snapshot)', () => {
+    const files = glob.sync('./test/result/v3/**/*.ts');
+    test.each(files.map(file => [file]))('file(%s)', file => {
+        const content = fs.readFileSync(file, 'utf8').toString();
+        expect(content).toMatchSnapshot(file);
     });
 });
