4a8db23dd3838b6f15ffc3fc463eb536740c0df2
Ferdi Koomen <info@madebyferdi.com>
2020-06-16 23:08:25 +0200

    - Not exporting unused exports

--- src/utils/writeClientServices.ts
+++ src/utils/writeClientServices.ts
@@ -5,6 +5,8 @@ import { writeFile } from './fileSystem';
 import { format } from './format';
 import { Templates } from './registerHandlebarTemplates';
 
+const VERSION_TEMPLATE_STRING = 'OpenAPI.VERSION';
+
 /**
  * Generate Services using the Handlebar template and write to disk.
  * @param services Array of Services to write.
@@ -15,8 +17,12 @@ import { Templates } from './registerHandlebarTemplates';
 export async function writeClientServices(services: Service[], templates: Templates, outputPath: string, useOptions: boolean): Promise<void> {
     for (const service of services) {
         const file = path.resolve(outputPath, `${service.name}.ts`);
+        const hasApiErrors = service.operations.some(operation => operation.errors.length);
+        const hasApiVersion = service.operations.some(operation => operation.path.includes(VERSION_TEMPLATE_STRING));
         const templateResult = templates.service({
             ...service,
+            hasApiErrors,
+            hasApiVersion,
             useOptions,
         });
         await writeFile(file, format(templateResult));
