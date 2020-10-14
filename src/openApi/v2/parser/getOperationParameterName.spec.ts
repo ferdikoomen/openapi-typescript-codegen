107895a7fd32347641eba6b21e0972e7a3d190f2
Ferdi Koomen <info@madebyferdi.com>
2020-10-15 00:15:13 +0200

    Better cleanup of service and operation names

--- src/openApi/v2/parser/getOperationParameterName.spec.ts
+++ src/openApi/v2/parser/getOperationParameterName.spec.ts
@@ -5,9 +5,12 @@ describe('getOperationParameterName', () => {
         expect(getOperationParameterName('')).toEqual('');
         expect(getOperationParameterName('foobar')).toEqual('foobar');
         expect(getOperationParameterName('fooBar')).toEqual('fooBar');
-        expect(getOperationParameterName('foo-bar')).toEqual('fooBar');
         expect(getOperationParameterName('foo_bar')).toEqual('fooBar');
+        expect(getOperationParameterName('foo-bar')).toEqual('fooBar');
         expect(getOperationParameterName('foo.bar')).toEqual('fooBar');
+        expect(getOperationParameterName('@foo.bar')).toEqual('fooBar');
+        expect(getOperationParameterName('$foo.bar')).toEqual('fooBar');
+        expect(getOperationParameterName('123.foo.bar')).toEqual('fooBar');
         expect(getOperationParameterName('Foo-Bar')).toEqual('fooBar');
         expect(getOperationParameterName('FOO-BAR')).toEqual('fooBar');
     });
