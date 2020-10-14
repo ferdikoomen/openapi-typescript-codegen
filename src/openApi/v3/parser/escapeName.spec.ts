85f1e8cc8e11d299d723fd59e15249568a505a78
Ferdi Koomen <info@madebyferdi.com>
2020-10-15 00:34:39 +0200

    Added unittests for escaping

--- src/openApi/v3/parser/escapeName.spec.ts
+++ src/openApi/v3/parser/escapeName.spec.ts
@@ -3,5 +3,19 @@ import { escapeName } from './escapeName';
 describe('escapeName', () => {
     it('should escape', () => {
         expect(escapeName('')).toEqual('');
+        expect(escapeName('fooBar')).toEqual('fooBar');
+        expect(escapeName('Foo Bar')).toEqual(`'Foo Bar'`);
+        expect(escapeName('foo bar')).toEqual(`'foo bar'`);
+        expect(escapeName('foo-bar')).toEqual(`'foo-bar'`);
+        expect(escapeName('foo.bar')).toEqual(`'foo.bar'`);
+        expect(escapeName('foo_bar')).toEqual('foo_bar');
+        expect(escapeName('123foo.bar')).toEqual(`'123foo.bar'`);
+        expect(escapeName('@foo.bar')).toEqual(`'@foo.bar'`);
+        expect(escapeName('$foo.bar')).toEqual(`'$foo.bar'`);
+        expect(escapeName('_foo.bar')).toEqual(`'_foo.bar'`);
+        expect(escapeName('123foobar')).toEqual(`'123foobar'`);
+        expect(escapeName('@foobar')).toEqual(`'@foobar'`);
+        expect(escapeName('$foobar')).toEqual('$foobar');
+        expect(escapeName('_foobar')).toEqual('_foobar');
     });
 });
