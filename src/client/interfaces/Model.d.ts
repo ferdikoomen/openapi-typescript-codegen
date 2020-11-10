a63db3774d0b3ccd63b2368d4f473ac1faac199c
Christian Budde Christensen <budde377@gmail.com>
2020-11-10 16:38:59 +0100

    feat: Improve support for allOf, oneOf, and anyOf

--- src/client/interfaces/Model.d.ts
+++ src/client/interfaces/Model.d.ts
@@ -3,7 +3,7 @@ import type { Schema } from './Schema';
 
 export interface Model extends Schema {
     name: string;
-    export: 'reference' | 'generic' | 'enum' | 'array' | 'dictionary' | 'interface';
+    export: 'reference' | 'generic' | 'enum' | 'array' | 'dictionary' | 'interface' | 'one-of' | 'any-of' | 'all-of';
     type: string;
     base: string;
     template: string | null;
