fe6a3be63ec5dd9f299917e9f82f084c3422eb32
Ferdi Koomen <info@madebyferdi.com>
2020-11-15 12:08:31 +0100

    - Fixed #411

--- test/e2e/v3.node.spec.js
+++ test/e2e/v3.node.spec.js
@@ -20,9 +20,20 @@ describe('v3.node', () => {
         const { OpenAPI, SimpleService } = require('./generated/v3/node/index.js');
         const tokenRequest = jest.fn().mockResolvedValue('MY_TOKEN')
         OpenAPI.TOKEN = tokenRequest;
+        OpenAPI.USERNAME = undefined;
+        OpenAPI.PASSWORD = undefined;
         const result = await SimpleService.getCallWithoutParametersAndResponse();
         expect(tokenRequest.mock.calls.length).toBe(1);
         expect(result.headers.authorization).toBe('Bearer MY_TOKEN');
+    });
+
+    it('uses credentials', async () => {
+        const { OpenAPI, SimpleService } = require('./generated/v3/node/index.js');
+        OpenAPI.TOKEN = undefined;
+        OpenAPI.USERNAME = 'username';
+        OpenAPI.PASSWORD = 'password';
+        const result = await SimpleService.getCallWithoutParametersAndResponse();
+        expect(result.headers.authorization).toBe('Basic dXNlcm5hbWU6cGFzc3dvcmQ=');
     });
 
     it('complexService', async () => {
