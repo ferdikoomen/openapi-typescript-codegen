3eb981cc57dd6652e4a0ebdbe80145129553c20d
Ferdi Koomen <info@madebyferdi.com>
2020-11-28 09:46:42 +0100

    - Filter empty composition models resolving "{...} & any" in extends

--- src/openApi/v2/parser/getModelComposition.ts
+++ src/openApi/v2/parser/getModelComposition.ts
@@ -15,11 +15,19 @@ export function getModelComposition(openApi: OpenApi, definitions: OpenApiSchema
     };
 
     const modes = definitions.map(definition => getModel(openApi, definition));
-    modes.forEach(model => {
-        composition.imports.push(...model.imports);
-        composition.enums.push(...model.enums);
-        composition.properties.push(model);
-    });
+    modes
+        .filter(model => {
+            const hasProperties = model.properties.length;
+            const hasEnums = model.enums.length;
+            const isObject = model.type === 'any';
+            const isEmpty = isObject && !hasProperties && !hasEnums;
+            return !isEmpty;
+        })
+        .forEach(model => {
+            composition.imports.push(...model.imports);
+            composition.enums.push(...model.enums);
+            composition.properties.push(model);
+        });
 
     return composition;
 }
