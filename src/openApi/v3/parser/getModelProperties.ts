3d46bb5188c58801aaff71c1ab092905b8644285
Henry Hsu <62437+hsume2@users.noreply.github.com>
2021-03-11 17:23:57 -0500

    WIP: Add axios client.

--- src/openApi/v3/parser/getModelProperties.ts
+++ src/openApi/v3/parser/getModelProperties.ts
@@ -2,9 +2,9 @@ import type { Model } from '../../../client/interfaces/Model';
 import { getPattern } from '../../../utils/getPattern';
 import type { OpenApi } from '../interfaces/OpenApi';
 import type { OpenApiSchema } from '../interfaces/OpenApiSchema';
-import { escapeName } from './escapeName';
 import { getComment } from './getComment';
 import type { getModel } from './getModel';
+import { getOperationParameterName } from './getOperationParameterName';
 import { getType } from './getType';
 
 // Fix for circular dependency
@@ -19,7 +19,7 @@ export function getModelProperties(openApi: OpenApi, definition: OpenApiSchema, 
             if (property.$ref) {
                 const model = getType(property.$ref);
                 models.push({
-                    name: escapeName(propertyName),
+                    name: getOperationParameterName(propertyName),
                     export: 'reference',
                     type: model.type,
                     base: model.base,
@@ -52,7 +52,7 @@ export function getModelProperties(openApi: OpenApi, definition: OpenApiSchema, 
             } else {
                 const model = getModel(openApi, property);
                 models.push({
-                    name: escapeName(propertyName),
+                    name: getOperationParameterName(propertyName),
                     export: model.export,
                     type: model.type,
                     base: model.base,
