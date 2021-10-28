5a7c58873f1870c5ad870938a73ec9c78b6cbbcb
Ferdi Koomen <info@madebyferdi.com>
2021-10-28 21:17:24 +0200

    - Fixed bundle when loading from url and resolving relative paths

--- src/utils/getOpenApiSpec.ts
+++ src/utils/getOpenApiSpec.ts
@@ -4,8 +4,8 @@ import RefParser from 'json-schema-ref-parser';
  * Load and parse te open api spec. If the file extension is ".yml" or ".yaml"
  * we will try to parse the file as a YAML spec, otherwise we will fallback
  * on parsing the file as JSON.
- * @param input
+ * @param location: Path or url
  */
-export async function getOpenApiSpec(input: string): Promise<any> {
-    return await RefParser.bundle(input);
+export async function getOpenApiSpec(location: string): Promise<any> {
+    return await RefParser.bundle(location, location, {});
 }
