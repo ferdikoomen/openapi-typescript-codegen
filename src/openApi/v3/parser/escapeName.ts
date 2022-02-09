e3421ddcd27baa5f60240d21b34835c67422013b
Slava Obukhov <vyobukhov@gmail.com>
2022-02-09 17:56:52 +0100

    Add support for empty model properties that are valid in OpenAPI

--- src/openApi/v3/parser/escapeName.ts
+++ src/openApi/v3/parser/escapeName.ts
@@ -1,5 +1,5 @@
 export const escapeName = (value: string): string => {
-    if (value) {
+    if (value || value === '') {
         const validName = /^[a-zA-Z_$][\w$]+$/g.test(value);
         if (!validName) {
             return `'${value}'`;
