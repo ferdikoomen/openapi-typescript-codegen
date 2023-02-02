5fb29a2ca9d7478e1565094c8b371ecd22daf5c1
Florian Schmidt <florian@f012.dev>
2023-02-02 20:30:43 +0100

    fix: parse and transform parameter style options correctly

--- src/openApi/v3/parser/getOperationParameter.ts
+++ src/openApi/v3/parser/getOperationParameter.ts
@@ -30,7 +30,7 @@ export const getOperationParameter = (openApi: OpenApi, parameter: OpenApiParame
         enums: [],
         properties: [],
         mediaType: null,
-        explode: parameter.explode || null,
+        explode: typeof parameter.explode === 'boolean' ? parameter.explode : null,
         style: (parameter.style as OperationParameter['style']) || null,
     };
 
