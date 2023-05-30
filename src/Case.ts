262a1bcf705bdd3847644301a1b6877115b17881
Oskar Asplin <oskarasplin@gmail.com>
2023-05-30 14:40:16 +0200

    Fix case conversion for service operation response body

--- src/Case.ts
+++ src/Case.ts
@@ -1,12 +1,14 @@
 import { Enum } from './client/interfaces/Enum';
 import { Model } from './client/interfaces/Model';
+import { OperationResponse } from './client/interfaces/OperationResponse';
+import { Service } from './client/interfaces/Service';
 
 export enum Case {
     NONE = 'none',
     CAMEL = 'camel',
     SNAKE = 'snake',
 }
-// Convert a string from snake case or pascal case to camel case.
+// Convert a string from snake case to camel case.
 const toCamelCase = (str: string): string => {
     return str.replace(/_([a-z])/g, match => match[1].toUpperCase());
 };
@@ -23,7 +25,7 @@ const transforms = {
 
 // A recursive function that looks at the models and their properties and
 // converts each property name using the provided transform function.
-export const convertModelNames = (model: Model, type: Exclude<Case, Case.NONE>): Model => {
+export const convertModelNames = <T extends Model | OperationResponse>(model: T, type: Exclude<Case, Case.NONE>): T => {
     return {
         ...model,
         name: transforms[type](model.name),
@@ -38,5 +40,15 @@ const convertEnumName = (modelEnum: Enum, type: Exclude<Case, Case.NONE>): Enum 
     return {
         ...modelEnum,
         name: transforms[type](modelEnum.name),
+    };
+};
+
+export const convertServiceCase = (service: Service, type: Exclude<Case, Case.NONE>): Service => {
+    return {
+        ...service,
+        operations: service.operations.map(op => ({
+            ...op,
+            results: op.results.map(results => convertModelNames(results, type)),
+        })),
     };
 };
--- src/utils/writeClient.ts
+++ src/utils/writeClient.ts
@@ -81,6 +81,7 @@ export const writeClient = async (
             useOptions,
             indent,
             postfixServices,
+            transformCase,
             clientName
         );
     }
