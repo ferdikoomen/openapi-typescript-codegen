d97edce97995f69b839265550580403cf8de0c7b
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-07-28 11:22:56 +0200

    Updated file system tests to be OS-agnostic - Test results for write operations are now designed to match irrespective of the system's path encoding: The previously used UNIX-style path strings do not match on Windows systems. Therefore, expected paths are now being resolved as they are in their corresponding write function. - Unit tests should now pass on all systems.

--- src/utils/writeClientSchemas.spec.ts
+++ src/utils/writeClientSchemas.spec.ts
@@ -1,4 +1,5 @@
 import { EOL } from 'os';
+import { resolve } from 'path';
 
 import type { Model } from '../client/interfaces/Model';
 import { HttpClient } from '../HttpClient';
@@ -53,6 +54,6 @@ describe('writeClientSchemas', () => {
 
         await writeClientSchemas(models, templates, '/', HttpClient.FETCH, false, Indent.SPACE_4);
 
-        expect(writeFile).toBeCalledWith('/$User.ts', `schema${EOL}`);
+        expect(writeFile).toBeCalledWith(resolve('/', '/$User.ts'), `schema${EOL}`);
     });
 });
