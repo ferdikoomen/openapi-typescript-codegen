683d796b3e934fc3709e9d0e2de6009175868c9c
Pavel Brylev <pavel@cytlex.com>
2023-08-14 10:52:21 +0300

    222222

---
+++ src/utils/__mocks__/fileSystem.ts
@@ -0,0 +1,6 @@
+export const readFile = jest.fn();
+export const writeFile = jest.fn();
+export const copyFile = jest.fn();
+export const exists = jest.fn();
+export const rmdir = jest.fn();
+export const mkdir = jest.fn();
---
+++ src/utils/__mocks__/templates.ts
@@ -0,0 +1,31 @@
+import type { Templates } from '../registerHandlebarTemplates';
+
+export const templates: Templates = {
+    index: () => 'index',
+    client: () => 'client',
+    exports: {
+        pathnames: {
+            pathname: () => 'pathname',
+            index: () => 'pathnameIndex',
+        },
+        factories: {
+            types: () => 'factoriesTypes',
+            index: () => 'factoriesIndex',
+        },
+        server: {
+            resolver: () => 'sererResolver',
+            index: () => 'serverIndex',
+        },
+        client: {
+            resolver: () => 'clientResolver',
+            index: () => 'clientIndex',
+        },
+        hook: {
+            resolver: () => 'hookResolver',
+            index: () => 'hookIndex',
+        },
+        model: () => 'model',
+        schema: () => 'schema',
+        service: () => 'service',
+    },
+};
