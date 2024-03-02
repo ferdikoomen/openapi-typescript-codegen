43a5786c926000358fff53070e9ceb6a27b0664f
Marek Lukáš <tajnymag@gmail.com>
2024-03-02 15:55:42 +0100

    Changed imports a bit to stop closer match project style

--- src/utils/getOpenApiSpec.ts
+++ src/utils/getOpenApiSpec.ts
@@ -1,6 +1,7 @@
 import RefParser from '@apidevtools/json-schema-ref-parser';
-import { exists } from 'fs-extra';
-import path from 'path';
+import { resolve } from 'path';
+
+import { exists } from './fileSystem';
 
 /**
  * Load and parse te open api spec. If the file extension is ".yml" or ".yaml"
@@ -9,6 +10,6 @@ import path from 'path';
  * @param location: Path or url
  */
 export const getOpenApiSpec = async (location: string): Promise<any> => {
-    const absolutePathOrUrl = (await exists(location)) ? path.resolve(location) : location;
+    const absolutePathOrUrl = (await exists(location)) ? resolve(location) : location;
     return await RefParser.bundle(absolutePathOrUrl, absolutePathOrUrl, {});
 };
